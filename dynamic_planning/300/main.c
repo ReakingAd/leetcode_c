2393b95fca9f0f5a7ee2b02dd18cc21c4997b706
Litao <litao3@hydsoft.com>
2024-04-15 02:00:09 +0800

    dp:300,增加贪心+二分算法

--- dynamic_planning/300/main.c
+++ dynamic_planning/300/main.c
@@ -36,7 +36,7 @@
  *  最后取dp[]数组的最大值就是题解。
  *
  */
-int lengthOfLIS(int* nums, int numsSize) {
+int lengthOfLIS_1(int* nums, int numsSize) {
     int dp[numsSize];
     dp[0] = 1; // 第一个元素的最长严格递增子序列就是自身，长度是1
     for(int i=1;i<numsSize;i++)
@@ -68,10 +68,130 @@ int lengthOfLIS(int* nums, int numsSize) {
     }
     return ans;
 }
+int lengthOfLIS_2(int* nums, int numsSize) {
+    int dp[numsSize];
+    for(int i=0;i<numsSize;i++) dp[i] = 1;
+    int ans = 1;
+    for(int i=1;i<numsSize;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(nums[j] < nums[i])
+            {
+                dp[i] = fmax(dp[i], dp[j] + 1);
+            }
+        }
+        ans = fmax(ans, dp[i]);
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        printf("%3d", nums[i]);
+    }
+    putchar('\n');
+    return ans;
+}
+// 二分查找
+int bi_search(int *nums, int numsSize, int target)
+{
+    int left = 0 ;
+    int right = numsSize - 1;
+    int mid;
+    while(left <= right)
+    {
+        mid = (left + right) / 2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] > target)
+        {
+            if(mid > 0 && target > nums[mid -1]) return mid;
+            right = mid -1;
+        }
+        else if(nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+    }
+    return mid;
+}
+/**
+ * LIS = Longest Increasing Subsequence 最长递增子序列
+ * 方法二： 贪心 + 二分查找。时间复杂度 o(nlogn)
+ * 贪心：要使最长子序列最长，那么每个放入第i个位置的元素都要尽可能小。
+ *
+ * arr[i] 存放构成长度为 i+1 的最长子序列的末位元素所能放的最小元素。
+ * nums[0]直接放入arr[0]遍历nums数组的1 ~ numsSize-1。
+ *      1. nums[i] > 当前arr的末位元素，则追加到arr末位
+ *      2. nums[i] < 当前arr的末位元素。则在arr[0] ~ arr[末位]中查找位置k，使满足arr[k-1] < nums[i] && arr[k] >= nums[i]
+ *        将arr[k]替换为nums[i]
+ * 最终arr的长度就是最长递增子序列的长度。
+ *
+ * ## 解析：
+ * 1. 注意：arr数组最终存放的不是最长递增子序列本身。
+ *    arr[i]位置上的元素就如上述定义中说的那样，仅仅是构成长度为i-1的最长递增子序列的末位元素所能放的最小值。
+ * 2. 维护这样一个arr有什么用？
+ *    设想输入数据为[1,2,3,4,1000, 5,6,7]。在nums中从前到后遍历数据组成LIS时，先组成了[1,2,3,4,1000]，
+ *    当对5进行决策时，5被1000挡住了。5既不能放在1000后面，又不能插入到1000前面（因为顺序不能颠倒）。
+ *    这样后续比1000小的数字，都被1000挡住了，就错过了组成更长的自增子序列的可能。因此，对于1000后面的元素，
+ *    当遇到能放在1000的位置上并且能构成LIS的元素，就应该替换掉1000，站在1000原本的位置上。
+ *
+ *    对于更一般的情况[1,3,4,7,1000, 2,5,6].在arr中用过大的数据占据位置的情况肯定不止1000一个数字。举例，当
+ *    遍历到数字2时，他不能追加到当前LIS的末位，那么就要用2去优化LIS前部分的元素，让构成当前长度的LIS的末位
+ *    元素是目前能选到的最小数字。
+ *
+ *    如此每个for循环到的元素，都对LIS的组成做了贡献。即使没有从末位插入，也优化了各个长度的LIS。为后续的数字
+ *    决策时，提供了最优的前提。
+ * 3. 时间复杂度o(nlogn)
+ *    当nums[i]不能插入到arr末位，让他去优化前部arr[0] ~ arr[末位-1]时，如果arr是个递增序列的话，使用二分查找
+ *    去找它应该优化的位置。二分的logn时间复杂度，显然要快于for循环的n。
+ *
+ *    前面分析可知维护的arr并不是最终的LIS，那么arr是否是个递增子序列呢？看两种情况下对arr的操作：
+ *    1. 追加到arr末位的操作，显然使arr保持递增
+ *    2. 查找 arr[k-1] < nums[i] && arr[k] >= nums[i] 的位置，并替换位置k显然也不破坏arr的递增
+ *    综上，arr是个递增序列，可以使用二分查找。
+ *
+ *
+ * ## 对比【双层for循环dp】与【贪心+二分】
+ * 【双层for循环dp】：当前状态下（遍历到nums[i]时），末位元素是定值，计算子序列长度最大值放入dp[i]
+ * 【贪心+二分】：当前状态下（遍历到nums[i]时）, 最长递增子序列长度是定值，计算末位元素的最小值放入arr[i]
+ *
+ * 可见，一个算法是是固定A，找B的最值。另一个算法是固定B，找A的最值
+ *
+ */
+int lengthOfLIS(int* nums, int numsSize) {
+    int arr[numsSize];
+    arr[0] = nums[0];
+    int len = 1;
+    for(int i=1;i<numsSize;i++)
+    {
+        if(nums[i] > arr[len-1])
+        {
+            arr[len] = nums[i];
+            len++;
+        }
+        else
+        {
+            int pos = bi_search(arr, len, nums[i]);
+            arr[pos] = nums[i];
+        }
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        printf("%3d", arr[i]);
+    }
+    putchar('\n');
+    return len;
+}
+
 int main() {
 //    int nums[8] = {10,9,2,5,3,7,101,18};
-    int nums[9] = {1,3,6,7,9,4,10,5,6};
-    int ans = lengthOfLIS(nums, 9);
+//    int nums[3] = {3,1,2};
+//    int nums[8] = {2, 3, 5, 7, 9,10,18, 101};
+//    int nums[6] = {0,1,0,3,2,3};
+        int nums[11] = {3,5,6,2,5,4,19,5,6,7,12};
+//    int nums[9] = {1,3,6,7,9,4,10,5,6};
+//    int pos = bi_search(nums, 8, 4);
+//    printf("pos=%d", pos);
+
+    int ans = lengthOfLIS(nums, 11);
     printf("ans=%3d\n", ans);
     return 0;
 }
